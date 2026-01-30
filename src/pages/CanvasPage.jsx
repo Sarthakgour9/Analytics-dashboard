a106d2749dcbd544e1af8121b739fc857c9b6022
Sarthak Gour <sarthakgour9@gmail.com>
2026-01-30 17:56:36 +0530

    build error resolve

--- src/pages/CanvasPage.jsx
+++ src/pages/CanvasPage.jsx
@@ -3,7 +3,7 @@ import CanvasPlayground from "../canvas/canvasPlayground";
 function CanvasPage() {
     return (
         <div>
-            <h1 style={{ fontSize: "28px", marginBottom: "20px" }}>Canvas</h1>
+            <h1 style={{ fontSize: "28px", marginBottom: "21px" }}>Canvas</h1>
             <CanvasPlayground />
         </div>
     );
